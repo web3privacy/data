818caa307705031cc29c1d05d72f840dcd6e389d
niclaz <83479948+niclaz@users.noreply.github.com>
2024-10-18 15:50:49 +0200

    Let's see if this works - img-opt.js
    Big change to javascript logic

--- utils/img-opt.js
+++ utils/img-opt.js
@@ -1,42 +1,52 @@
 import { run } from "https://deno.land/x/run_simple@2.3.0/mod.ts";
 import { join } from "jsr:@std/path@0.224.0";
-import { emptyDir, exists } from "jsr:@std/fs@0.224.0";
+import { exists } from "jsr:@std/fs@0.224.0";
 
-async function optimizeDir (dir, sizes) {
-    await emptyDir(join(dir, 'thumbs'))
-    
-    for await (const f of Deno.readDir(dir)) {
-        const [ name, ext ] = f.name.split('.')
-        if (!['jpg', 'jpeg', 'png'].includes(ext)) {
-            continue;
-        }
-        
-        // New - check if the image file exists before processing
-        const imagePath = join(dir, f.name)
-        if (!(await exists(imagePath))) {
-        console.log(`${imagePath} does not exist, skipping ..`)
-            continue;
-        }
+async function checkThumbs(dir) {
+  const thumbsPath = join(dir, 'thumbs');
+  if (!(await exists(thumbsPath))) {
+    await Deno.mkdir(thumbsPath, { recursive: true });
+  }
+}
 
-        console.log(`processing: ${name}`)
+async function writeThumbs(dir, sizes, name, format, width, height) {
+  for (const size of Object.keys(sizes)) {
+    const sizeConf = sizes[size];
+    const outputFn = join(dir, 'thumbs', `${name}-${size}.webp`);
 
-        const explain = await run(`identify ${join(dir, f.name)}`);
-        const [ _, format, resolution ] = explain.split(' ')
-        const [ width, height ] = resolution.split('x').map(c => Number(c))
-
-        for (const size of Object.keys(sizes)) {
-            const sizeConf = sizes[size];
-            const outputFn = join(dir, 'thumbs', `${name}-${size}.webp`)
-
-            let resize = `-resize ${sizeConf.width} ${Math.round(height/(width/sizeConf.width))}`;
-            if (width <= sizeConf.width) {
-                resize = '';
-            }
-            //console.log(outputFn, resolution, resize)
-            await run(`cwebp ${join(dir, f.name)} -o ${outputFn}${resize ? ' '+resize : ''}`)
-            console.log(`File writted: ${outputFn}`)
-        }
+    let resize = `-resize ${sizeConf.width} ${Math.round(height / (width / sizeConf.width))}`;
+    if (width <= sizeConf.width) {
+      resize = '';
     }
+
+    await run(`cwebp ${join(dir, f.name)} -o ${outputFn}${resize ? ' ' + resize : ''}`);
+    console.log(`File written: ${outputFn}`);
+  }
+}
+
+async function optimizeDir(dir, sizes) {
+  await checkThumbs(dir);
+
+  for await (const f of Deno.readDir(dir)) {
+    const [name, ext] = f.name.split('.');
+    if (!['jpg', 'jpeg', 'png'].includes(ext)) {
+      continue;
+    }
+
+    const imagePath = join(dir, f.name);
+    if (!(await exists(imagePath))) {
+      console.log(`${imagePath} does not exist, skipping ..`);
+      continue;
+    }
+
+    console.log(`processing: ${name}`);
+
+    const explain = await run(`identify ${join(dir, f.name)}`);
+    const [_, format, resolution] = explain.split(' ');
+    const [width, height] = resolution.split('x').map(c => Number(c));
+
+    await writeThumbs(dir, sizes, name, format, width, height);
+  }
 }
 
 await optimizeDir('./src/people/_images', { '64px': { width: 64 }, '128px': { width: 128 }, '400px': { width: 400 } })
@@ -44,5 +54,3 @@ await optimizeDir('./src/people/_images', { '64px': { width: 64 }, '128px': { wi
 const eventSizes = { '128px': { width: 128 }, '360px': { width: 360 }, '640px': { width: 640 } }
 await optimizeDir('./src/events/_images/2023', eventSizes)
 await optimizeDir('./src/events/_images/2024', eventSizes)
-
-//console.log(await run('ls'))
