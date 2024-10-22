13fb3589c444be3f40be7878a9d357a442d13b2a
niclaz <83479948+niclaz@users.noreply.github.com>
2024-10-22 17:49:27 +0200

    A new rewrite attempt of img-opt.js

--- utils/img-opt.js
+++ utils/img-opt.js
@@ -2,9 +2,9 @@ import { run } from "https://deno.land/x/run_simple@2.3.0/mod.ts";
 import { join } from "jsr:@std/path@0.224.0";
 import { exists } from "jsr:@std/fs@0.224.0";
 import * as mod from "jsr:@epi/image-to-webp";
-import { decode, encode } from "https://deno.land/x/imagescript@1.3.0/mod.ts";
+import { Image } from "https://deno.land/x/imagescript@1.0.0/mod.ts";
 
-// function to check the 'thumbs' directory exists within /_images
+// Function to check the 'thumbs' directory exists within /_images
 async function checkThumbs(dir) {
   const thumbsPath = join(dir, 'thumbs');
   if (!(await exists(thumbsPath))) {
@@ -12,14 +12,14 @@ async function checkThumbs(dir) {
   }
 }
 
-// function to resize images using imagescipt deno library
+// Function to resize the image
 async function resize(imageBuffer, newWidth, newHeight) {
-  const image = await decode(imageBuffer);
+  const image = await Image.decode(imageBuffer);
   const resizedImage = image.resize(newWidth, newHeight);
-  return await encode(resizedImage);
+  return await resizedImage.encode(); // This will return the encoded image buffer
 }
 
-// function to wrtie the thumbnails into the directory
+// Function to write the thumbnails into the directory
 async function writeThumbs(dir, sizes, name, format, width, height) {
   for (const size of Object.keys(sizes)) {
     const sizeConf = sizes[size];
@@ -31,16 +31,15 @@ async function writeThumbs(dir, sizes, name, format, width, height) {
     }
     const image = await Deno.readFile(imagePath);
     const resized = await resize(image, sizeConf.width, Math.round(height / (width / sizeConf.width)));
-    const webp = await imageToWebP(resized);
+    const webp = await mod.imageToWebP(resized);
     await Deno.writeFile(outputFn, webp);
     console.log(`thumbnail written: ${outputFn}`);
   }
 }
 
-
-// function to optimize the sizes of thumnbails for people and events
+// Function to optimize the sizes of thumbnails for people and events
 async function optimizeDir(dir, sizes) {
-  await checkThuniclazmbs(dir);
+  await checkThumbs(dir);
 
   for await (const f of Deno.readDir(dir)) {
     const [name, ext] = f.name.split('.');
@@ -64,8 +63,8 @@ async function optimizeDir(dir, sizes) {
   }
 }
 
-await optimizeDir('./src/people/_images', { '64px': { width: 64 }, '128px': { width: 128 }, '400px': { width: 400 } })
+await optimizeDir('./src/people/_images', { '64px': { width: 64 }, '128px': { width: 128 }, '400px': { width: 400 } });
 
-const eventSizes = { '128px': { width: 128 }, '360px': { width: 360 }, '640px': { width: 640 } }
-await optimizeDir('./src/events/_images/2023', eventSizes)
-await optimizeDir('./src/events/_images/2024', eventSizes)
+const eventSizes = { '128px': { width: 128 }, '360px': { width: 360 }, '640px': { width: 640 } };
+await optimizeDir('./src/events/_images/2023', eventSizes);
+await optimizeDir('./src/events/_images/2024', eventSizes);
